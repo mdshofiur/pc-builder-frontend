9550549a66bae3fefe9dd6a7c69bcc981a32545e
Md Shafikur Rahaman <58303621+mdshofiur@users.noreply.github.com>
2023-07-30 16:29:44 +0600

    button text change

--- components/product-card.tsx
+++ components/product-card.tsx
@@ -56,7 +56,7 @@ const ProductCard = ({ product }: { product: Product }) => {
             <button
                onClick={handleSelect}
                className='block mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600'>
-               Select
+               Add To Builder
             </button>
          )}
       </div>
